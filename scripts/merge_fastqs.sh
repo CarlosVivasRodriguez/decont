5df51de3084c5d138c52f9290f4d0aca8a92e830
Carlos Vivas <carvivas1@gmail.com>
2024-01-18 10:55:04 +0100

    Archivos Modificados

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+# Check if the correct number of arguments is provided
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <samples_directory> <output_directory> <sample_id>"
+    exit 1
+fi
+
+samples_directory="$1"
+output_directory="$2"
+sample_id="$3"
+
+mkdir -p "$output_directory"
+
+cat "$samples_directory"/"$sample_id" > "$output_directory/merged_sample_${sample_id}.txt"
+
+echo "Files merged successfully!"
 # This script should merge all files from a given sample (the sample id is
 # provided in the third argument ($3)) into a single file, which should be
 # stored in the output directory specified by the second argument ($2).
