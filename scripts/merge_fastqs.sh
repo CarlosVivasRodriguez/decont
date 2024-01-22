468ef3b63a445bab8f32b613741256cac1e33ac0
Carlos Vivas <carvivas1@gmail.com>
2024-01-22 16:50:12 +0100

    "Ultimas modificaciones de scripts 22/01"

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -1,21 +1,13 @@
-#!/bin/bash
-
-# Check if the correct number of arguments is provided
+bash scripts/index.sh res/filtered_contaminants.fasta res/filtered_contaminants_idx
 if [ "$#" -lt 3 ]; then
     echo "Usage: $0 <samples_directory> <output_directory> <sample_id>"
     exit 1
 fi
 
-samples_directory="$1"
-output_directory="$2"
-sample_id="$3"
+samples_directory=$1
+output_directory=$2
+sample_id=$3
 
-mkdir -p "$output_directory"
+mkdir -p $output_directory
 
-cat "$samples_directory"/"$sample_id" > "$output_directory/${sample_id}.fastq.gz"
-
-# This script should merge all files from a given sample (the sample id is
-# provided in the third argument ($3)) into a single file, which should be
-# stored in the output directory specified by the second argument ($2).
-#
-# The directory containing the samples is indicated by the first argument ($1).
+cat $samples_directory/${sample_id}-12.5dpp.1.1s_sRNA.fastq.gz $samples_directory/${sample_id}-12.5dpp.1.2s_sRNA.fastq.gz > $output_directory/${sample_id}.fastq.gz
